3a2da939e370f2208d8244cb3dab77d80e140182
Burak Furkan ERKEMEN <84676805+BFEisHERE@users.noreply.github.com>
2022-03-28 01:47:40 +0300

    Add files via upload

---
+++ bölüm7/bolum7_modul1.py
@@ -0,0 +1,8 @@
+#********KENDİ MODÜLÜNÜ YAZMAK!!!!********
+Programlama_dilleri = ["Python","PHP","Java","c"]
+
+def toplama(a,b,c):
+       return a+ b +c
+
+def selamla(isim):
+       print("Selam",isim)
---
+++ bölüm7/modul.py
@@ -0,0 +1,9 @@
+import bolum7_modul1
+
+print(bolum7_modul1.toplama(3,4,5))
+
+bolum7_modul1.selamla("Burak")
+"""
+python35 klasörüne atıldığı takdirde istenilen zamanda kullaıma hazır
+bir şekilde emrinize amade bir modül olur
+"""
